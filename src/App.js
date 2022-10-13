9b6c164f1b22a939934a2fe4bd749ae9c445ca1e
Sumio K <tap.miwo@gmail.com>
2022-10-14 06:37:33 +0900

    install yarn mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      hello, react
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SidebarData } from './SidebarData'
+
+function Sidebar() {
+  return <div className='Sidebar'>
+    <ul className='SidebarList'>
+      
+    </ul>
+  </div>
+}
+
+export default Sidebar
