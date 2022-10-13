566ff3e2ace0adc06c938b24933568534f82788a
Sumio K <tap.miwo@gmail.com>
2022-10-14 06:54:48 +0900

    shape of list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,18 @@ import { SidebarData } from './SidebarData'
 function Sidebar() {
   return <div className='Sidebar'>
     <ul className='SidebarList'>
-      
+      {SidebarData.map((value, key) => {
+        return(
+          <li key = {key} className="row" onClick={() => {
+            window.location.pathname = value.link;
+          }}>
+            <div id="icon">{value.icon}</div>
+
+            <div id="title">{value.title}</div>
+
+          </li>
+        )
+      })}
     </ul>
   </div>
 }
