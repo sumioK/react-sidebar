7312818413c2c99c84cce28be8c831edd12db344
Sumio K <tap.miwo@gmail.com>
2022-10-14 22:13:09 +0900

    done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { SidebarData } from './SidebarData'
+import SidebarIcon from './SidebarIcon'
 
 function Sidebar() {
-  return <div className='Sidebar'>
+  return (
+  <div className='Sidebar'>
+    <SidebarIcon />
     <ul className='SidebarList'>
       {SidebarData.map((value, key) => {
         return(
-          <li key = {key} className="row" onClick={() => {
+          <li 
+          key = {key}
+          id = {window.location.pathname == value.link ? "active" : ""}
+          className="row" 
+          onClick={() => {
             window.location.pathname = value.link;
           }}>
             <div id="icon">{value.icon}</div>
@@ -18,6 +25,7 @@ function Sidebar() {
       })}
     </ul>
   </div>
+  )
 }
 
 export default Sidebar
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Icon from './images/icon.png'
+
+function SidebarIcon() {
+  return (
+    <div className='SidebarIcon'>
+      <img src={Icon} />
+      <p>miwo@gmail.com</p>
+    </div>
+    
+  )
+}
+
+export default SidebarIcon
